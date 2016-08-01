7e29d870f5561660995de6d564dc511dddf7e1f4
Sv.Potaychuk <sviatoslavpotaychuk@gmail.com>
2016-08-01 08:17:08 +0300

    asdasd

--- src/main/java/main/resources/CREATE_daotalk_sub_services.sql
+++ src/main/java/main/resources/CREATE_daotalk_sub_services.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS sub_services;
-CREATE TABLE sub_services
+CREATE TABLE daotalk.sub_services
 (
     sub_id INT(11),
-    service_id INT(11),
+    service_id INT(11)DEFAULT '0',
     deleted TINYINT(1) DEFAULT '0'
 );
