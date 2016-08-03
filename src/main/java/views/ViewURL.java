ed2b2e92c6a32e1f15dba2d8fe456c53790c672c
Sv.Potaychuk <sviatoslavpotaychuk@gmail.com>
2016-08-03 23:36:50 +0300

    qwe

--- src/main/java/views/ViewURL.java
+++ src/main/java/views/ViewURL.java
@@ -5,13 +5,14 @@ package views;
  */
 public class ViewURL {
     public static final String HOME_JSP = "/view/Home.jsp";
-    public static final String ADMIN_HOME_JSP = "/view/secure/Admin.jsp";
+
     public static final String INDEX_JSP = "index.jsp";
     public static final String CREATE_SUB_JSP = "/view/CreateSub.jsp";
-    public static final String TEST_JSP = "/view/TestJsp.jsp";
     public static final String SERVICE_LIST_JSP = "/view/ServiceList.jsp";
     public static final String FILL_BALANCE_JSP = "/view/FillBalance.jsp";
     public static final String DIRECT_INFO_JSP = "/view/DirectInfo.jsp";
+
+    public static final String ADMIN_HOME_JSP = "/view/secure/Admin.jsp";
     public static final String USER_LIST_JSP = "/view/secure/UserList.jsp";
     public static final String SERVICE_LIST_EDIT_JSP = "/view/secure/ServiceListEdit.jsp";
 }
